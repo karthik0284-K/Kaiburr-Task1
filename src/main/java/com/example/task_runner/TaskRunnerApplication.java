fcb4c46b819c45cef19cc4372a62f13312178cc2
Karthik <karthikkusuma002@gmail.com>
2025-10-21 13:34:26 +0530

    Initial commit

---
+++ src/main/java/com/example/task_runner/TaskRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_runner/TaskRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
